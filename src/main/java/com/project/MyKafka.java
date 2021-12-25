5e5d584d64bbe480063b7fc69d8decead97d8729
MeroMemo <1043526080@qq.com>
2021-12-25 14:15:29 +0800

    first kafka begin

---
+++ src/main/java/com/project/MyKafka.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: hxd
+ * @Date: 2021/12/23 11:34 下午
+ */
+@SpringBootApplication
+public class MyKafka {
+    public static void main(String[] args) {
+        SpringApplication.run(MyKafka.class, args);
+    }
+}
