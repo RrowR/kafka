48ad8f32d53a208d096283c546fe56c847828b04
Memo <2415782224@qq.com>
2022-01-27 01:48:23 +0800

    kafka的基本使用(生产和消费)

---
+++ src/main/java/com/project/consumer/ComsumerMsg.java
@@ -0,0 +1,46 @@
+package com.project.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * atlan 2022/1/26 18:14
+ */
+public class ComsumerMsg {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        // Kafka服务器
+        props.setProperty("bootstrap.servers", "ecs01:9092");
+        /*
+            消费者组，如果消费者组名是一样的，那么就可以消费同一份数据，可以起多个
+         */
+        props.setProperty("group.id", "test");
+        // 自动提交offset(偏移量)
+        props.setProperty("enable.auto.commit", "true");
+        // 自动提交offset的提交间隔
+        props.setProperty("auto.commit.interval.ms", "1000");
+        // 解析格式
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        // 指定从哪个topic里消费
+        consumer.subscribe(Arrays.asList("kkk"));
+        // 死循环，主动去拉数据
+        while (true) {
+            // KafkaConsumer.poll(final Duration timeout)   设置超时时间，拉的是一批数据，实现了Iterable接口
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records){
+                // 从哪个主题里拉取出来
+                System.out.println("record.topic() = " + record.topic());
+                System.out.println("record.offset() = " + record.offset());
+                System.out.println("record.key() = " + record.key());
+                System.out.println("record.value() = " + record.value());
+            }
+        }
+    }
+}
--- src/main/java/com/project/producer/ProduceMsg.java
+++ src/main/java/com/project/producer/ProduceMsg.java
@@ -3,29 +3,38 @@ package com.project.producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @Author: Rrow
  * @Date: 2022/1/26 4:37 下午
  */
+
+/**
+ * 生产者程序,注意，这里要配置server.properties文件的 advertised.listeners=PLAINTEXT://xxx.xxx.xxx.xxx:9092 在zookeeper的外部暴露端口
+ */
 public class ProduceMsg {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
         props.put("bootstrap.servers", "ecs01:9092");
         props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
         for (int i = 0; i < 100; i++) {
-            producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
+            ProducerRecord<String, String> record = new ProducerRecord<>("kkk", null, i + "");
+            Future<RecordMetadata> future = producer.send(record);
+            // 等待响应
+            System.out.println("future.get() = " + future.get());
+            System.out.println("第" + i + "条消息，写入成功!!");
         }
+        // 关闭生产者
         producer.close();
+
     }
 }
